d4fad55a38058ed0e3e5b3704eea1218c57bc4fc
Khalid Ajlan <khalidhh85@gmail.com>
2019-10-04 22:07:31 +0300

    Initial Commit

---
+++ TextFieldChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldChallenge
+//
+//  Created by Khalid Ajlan on 10/4/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
