1b7ec057378d51486863c3fa94b5f21c70a4fdde
Khalid Ajlan <khalidhh85@gmail.com>
2019-10-05 01:35:13 +0300

    Created new class for the switch text field

---
+++ TextFieldChallenge/TextField/SwitchTextField.swift
@@ -0,0 +1,14 @@
+//
+//  SwitchTextField.swift
+//  TextFieldChallenge
+//
+//  Created by Khalid Ajlan on 10/5/19.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwitchTextField: NSObject, UITextFieldDelegate{
+    
+}
